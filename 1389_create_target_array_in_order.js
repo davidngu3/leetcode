080d9c9c032154ab51061a0049e28494d883c8c4
davidngu3 <d.nguyen.melb@gmail.com>
2020-04-11 14:16:43 +1000

    problems 1389

--- 1389_create_target_array_in_order.js
+++ 1389_create_target_array_in_order.js
@@ -30,5 +30,9 @@
  * @return {number[]}
  */
 var createTargetArray = function(nums, index) {
-    
+    var target = [];
+    for (let i=0; i < nums.length; i++) {
+        target.splice(index[i], 0, nums[i]);
+    }
+    return target;
 };
