4b0122302efc075157c7228f42d3f06131f75236
davidngu3 <d.nguyen.melb@gmail.com>
2020-07-17 14:32:11 +1000

    1047 stack

--- 1047_remove_adjacent_duplicates.js
+++ 1047_remove_adjacent_duplicates.js
@@ -3,23 +3,13 @@
  * @return {string}
  */
 var removeDuplicates = function(S) {
-
-    while (findDuplicate(S) !== null) {
-        let dIndex = findDuplicate(S)
-        S = S.replace(S.substring(dIndex, dIndex+2), "");
-    }
-
-    return S;
-};
-
-// returns the first index which is succeeded by a duplicate letter
-function findDuplicate(S) {
-    for (let i=1; i<S.length; i++) {
-        if (S[i] == S[i-1]) {
-            return i-1;
+    let res = [];
+    for(let i=0; i<S.length; i++){
+        if(S[i] !== res[res.length-1]) {
+            res.push(S[i]);
+        } else {
+            res.pop();
         }
     }
-    return null;
-}
-
-removeDuplicates("abbaca");
+    return res.join("");
+};
