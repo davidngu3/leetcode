b748bb2e163ab677c8c941f15fbd30301f8253d2
davidngu3 <d.nguyen.melb@gmail.com>
2020-07-19 13:44:38 +1000

    344 using reverse function

--- 344_reverse_string.js
+++ 344_reverse_string.js
@@ -3,8 +3,5 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let maxIndex = s.length-1;
-    for (let i=0; i<=Math.floor(maxIndex/2); ++i) {
-        [s[i], s[maxIndex-i]] = [s[maxIndex-i], s[i]];
-    }
+    s.reverse();
 };
