357b92446026fcbba0267087fe92d8a23f4ffcf1
davidngu3 <d.nguyen.melb@gmail.com>
2020-07-28 11:16:54 +1000

    509 using memoization

--- 509_fibonacci.js
+++ 509_fibonacci.js
@@ -3,11 +3,17 @@
  * @return {number}
  */
 var fib = function(N) {
-    if (N == 0) {
-        return 0;
-    }
-    if (N == 1) {
-        return 1;
-    }
-    return fib(N-1) + fib(N-2);
+    return memoize(N);
 };
+
+function memoize(N) {
+    var memo = new Array(N+1);
+    memo[0] = 0;
+    memo[1] = 1;
+    
+    for (let i = 2; i < N + 1; i++) {
+        memo[i] = memo[i-1] + memo[i-2];
+    }
+    
+    return memo[N];
+}
