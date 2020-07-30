d6407c10e88d198a20a16d3c4d93213113219898
davidngu3 <d.nguyen.melb@gmail.com>
2020-07-30 16:44:10 +1000

    821 with fancier js operator

--- 821_shortest_distance_to_char.js
+++ 821_shortest_distance_to_char.js
@@ -4,13 +4,7 @@
  * @return {number[]}
  */
 var shortestToChar = function(S, C) {
-    var indexArray = [];
-    
-    for (let i=0; i<S.length; i++) {
-        if (S[i] == C) {
-            indexArray.push(i);
-        }
-    }
+    var indexArray = S.split('').map((e, i) => e == C ? i : null).filter(e => e == null ? false : true);
     
     return S.split('').map((e, i) => {
         return Math.min(...indexArray.map(d => Math.abs(d - i)));
