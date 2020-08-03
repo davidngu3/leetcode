c07c3a771d833b2a676c287b8d283185fb8b344d
davidngu3 <d.nguyen.melb@gmail.com>
2020-08-03 15:49:24 +1000

    1025 after figuring out the game

--- 1025_divisor_game.js
+++ 1025_divisor_game.js
@@ -3,36 +3,16 @@
  * @return {boolean}
  */
 var divisorGame = function(N) {
-    var DP = new Array(N+1);
-    DP[1] = false; // base case: with a starting value of 1, alice loses because she has no turn
-    
-    for (let i=2; i<=N; i++) {
-        var divisors = findDivisors(i);
-        var success = false;
-        
-        var j = 0;
-        while (!success && j<divisors.length) {
-            if (DP[i-divisors[j]] == false) {
-                success = true;
-                DP[i] = true;
-            } 
-            j++;
-        }
-        if (!success) {
-            DP[i] = false;
-        }
-        
-    }
-    
-    return DP[N];
+    return N % 2 == 0 ? true : false;
 };
 
-function findDivisors(n) {
-    var ret = [1];
-    for (let i=2; i<=Math.floor(n/2); ++i) {
-        if (n % i == 0) {
-            ret.push(i);
-        }
-    }
-    return ret;
-}
+// WHY IS THIS THE CASE?
+// In this game, alice always goes first and subtracts a divisor of N from N.
+// The base value is 1, which alice will always lose.
+// Thus on N = 2, alice can win simply by subtracting 1 and leaving bob with the base case of 1
+// on N = 3, alice loses because she can only subtract 1, which leaves Bob starting with N = 2, which we proved he wins in the last line
+// on N = 4, alice wins because she can simply subtract 1 to leave bob with the losing case from the last line.
+// on N = 5, alice loses because she can only subtract 1, which leaves Bob starting with N = 4, which we proved he wins in the last line.
+// ...
+// In general, if N is even, alice will win because she can subtract 1 to make N odd
+// When N is odd, the player must subtract a divisor of N. Divisors of N are always odd, so N will always end up odd. The first odd N was 1, which is false, so alice will always lose on odd N.
