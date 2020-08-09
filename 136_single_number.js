ac236a9568e560500e7ebea1f2a116228427c501
davidngu3 <d.nguyen.melb@gmail.com>
2020-08-09 19:16:16 +1000

    136 using sorting

---
+++ 136_single_number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    var sorted = nums.sort((a, b) => a-b);
+    for (let i=0; i<sorted.length; i+=2) {
+        if (sorted[i] !== sorted[i+1]) {
+            return sorted[i];
+        }
+    }
+};
