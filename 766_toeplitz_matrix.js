e36ed06de590d7febf61069281410166d622d446
davidngu3 <d.nguyen.melb@gmail.com>
2020-08-11 13:29:19 +1000

    766 2 loops

---
+++ 766_toeplitz_matrix.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    for (let i = 0; i < matrix[0].length; i++) {
+        var diagonal = matrix[0][i];
+        
+        var row = 1;
+        var col = i+1;
+        
+        while (row < matrix.length && col < matrix[0].length) {
+            if (matrix[row][col] !== diagonal) {
+                return false;
+            }
+            row++;
+            col++;
+        }
+    }
+    
+    for (let i = 1; i < matrix.length; i++) {
+        var diagonal = matrix[i][0];
+        
+        var row = i+1;
+        var col = 1;
+        
+        while (row < matrix.length && col < matrix[0].length) {
+            if (matrix[row][col] !== diagonal) {
+                return false;
+            }
+            row++;
+            col++;
+        }
+    }
+    
+    return true;
+};
