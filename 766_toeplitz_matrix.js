dbc27902431f1cee4bf0ee100a9ff4f0941852e7
davidngu3 <d.nguyen.melb@gmail.com>
2020-08-11 13:43:39 +1000

    766 by comparing top left neighbour

--- 766_toeplitz_matrix.js
+++ 766_toeplitz_matrix.js
@@ -3,28 +3,13 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    var startRows = [];
-    for (let i = 0; i < matrix[0].length - 1; i++) {
-        startRows.push([0, i]);
-    }
-    
-    for (let j = 1; j < matrix.length - 1; j++) {
-        startRows.push([j, 0]);
-    }
-    
-    for (point of startRows) {
-        row = point[0]; col = point[1];
-        var plitzVal = matrix[row][col]
-        row++; col++;
-        
-        while (row < matrix.length && col < matrix[0].length) {
-            if (matrix[row][col] !== plitzVal) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (i > 0 && j > 0 && matrix[i-1][j-1] != matrix[i][j]) {
                 return false;
-            }
-            row++;
-            col++;
+            }  
         }
     }
-    
+
     return true;
 };
