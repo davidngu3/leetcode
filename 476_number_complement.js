2f00164a0398250a0a400b63ebc79da87e2e00d1
davidngu3 <d.nguyen.melb@gmail.com>
2020-08-15 20:35:38 +1000

    476 with js syntactic sugar

--- 476_number_complement.js
+++ 476_number_complement.js
@@ -3,11 +3,7 @@
  * @return {number}
  */
 var findComplement = function(num) {
-    let bin = num.toString(2);
-    let ret = "";
-    for (let i = 0; i<bin.length; i++) {
-        ret += (bin[i] == '0' ? '1' : '0');
-    }
+    let bin = num.toString(2).split('');
     
-    return parseInt(ret, 2);
+    return parseInt(bin.map(e => e == 0 ? 1 : 0).join(''), 2);
 };
