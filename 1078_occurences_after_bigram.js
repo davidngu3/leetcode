67804cd1c8ea92d4787e7029533446795e1475c1
davidngu3 <d.nguyen.melb@gmail.com>
2020-08-17 13:10:32 +1000

    1078 one liner using filter

--- 1078_occurences_after_bigram.js
+++ 1078_occurences_after_bigram.js
@@ -5,16 +5,5 @@
  * @return {string[]}
  */
 var findOcurrences = function(text, first, second) {
-    let occurences = [];
-    let sentence = text.split(" ");
-    
-    for (let i = 0; i < sentence.length - 2; i++) {
-        if (sentence[i] == first) {
-            if (sentence[i+1] == second) {
-                occurences.push(sentence[i+2]);
-            }
-        }
-    }
-    
-    return occurences;
+    return text.split(" ").filter((e, i, arr) => arr[i-2] == first && arr[i-1] == second);
 };
