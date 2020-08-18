b6f899442906aa503e47efe1dede1e55755a2292
davidngu3 <d.nguyen.melb@gmail.com>
2020-08-18 11:41:29 +1000

    1455 shorter

--- 1455_check_if_word_occurs_as_prefix.js
+++ 1455_check_if_word_occurs_as_prefix.js
@@ -6,17 +6,10 @@
 var isPrefixOfWord = function(sentence, searchWord) {
     var words = sentence.split(" ");
     for (let i = 0; i < words.length; i++) {
-        if (hasPrefix(words[i], searchWord)) {
+        if (words[i].startsWith(searchWord)) {
             return i+1;
         }    
     }
     return -1;
 };
 
-var hasPrefix = function(word, prefix) {
-    console.log(word + " " + prefix);
-    if (word.startsWith(prefix)) {
-        return true;
-    }
-    return false;
-}
