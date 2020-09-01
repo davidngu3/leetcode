05e5cbbf7524e237e647c70575c0c104a24e69cb
davidngu3 <d.nguyen.melb@gmail.com>
2020-09-01 12:14:25 +1000

    867 creating empty matrix and swapping indices

---
+++ 867_transpose_matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var transpose = function(A) {
+    var ret = new Array(A[0].length).fill().map(e => new Array(A.length).fill());
+    
+    for (let r = 0; r < A.length; r++ ) {
+        for (let c = 0; c < A[0].length; c++ ) {
+            console.log('row' + r + 'col' + c);
+            ret[c][r] = A[r][c];
+        }
+    }
+    
+    return ret;
+}; 
