1d1ba9f9a4ea4d6fb24e7066af99c5ffd25477a9
davidngu3 <d.nguyen.melb@gmail.com>
2020-09-03 12:53:22 +1000

    706 using object

---
+++ 706_design_hash.js
@@ -0,0 +1,46 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function() {
+    this.hash = {};
+    
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this.hash[key] = value;
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    if (!this.hash.hasOwnProperty(key)) {
+        return -1;
+    }
+    return this.hash[key];
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+    delete this.hash[key];
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
