e5f7851ed56e68a0bd0215c54790586fdfbe3d78
davidngu3 <d.nguyen.melb@gmail.com>
2020-09-06 13:33:22 +1000

    1260 using modulo

--- 1260_shift_2d_grid.js
+++ 1260_shift_2d_grid.js
@@ -11,7 +11,9 @@ var shiftGrid = function(grid, k) {
     
     for (let r = 0; r < grid.length; r++) {
         for (let c = 0; c < grid[0].length; c++) {
-            [newRow, newCol] = shiftNum(r, c, grid.length-1, grid[0].length-1, k);
+            var newCol = (c + k) % grid[0].length;
+            var rowIncrements = Math.floor((c + k) / grid[0].length);
+            var newRow = (r + rowIncrements) % grid.length;
             ret[newRow][newCol] = grid[r][c];
         }
     }
@@ -20,21 +22,3 @@ var shiftGrid = function(grid, k) {
 };
 
 
-function shiftNum(r, c, rmax, cmax, k) {
-    while (k > 0) {
-        if (r == rmax && c == cmax) { // end of grid; reset to [0, 0]
-            r=0;
-            c=0;
-        }
-        else if (c == cmax) { // end of row; reset col, increment row
-            r++;
-            c=0;
-        }
-        else {
-            c++; // shift by 1
-        }
-        k--; // decrement shift counter
-    }
-    
-    return [r, c];
-}
