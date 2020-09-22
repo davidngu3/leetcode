90e996f22ffa429ffef9f3b543b9db66b3d58ba3
davidngu3 <d.nguyen.melb@gmail.com>
2020-09-22 15:51:45 +1000

    200 optimise with dfs, remove mem arr

--- 200_number_of_islands.js
+++ 200_number_of_islands.js
@@ -2,57 +2,32 @@
  * @param {character[][]} grid
  * @return {number}
  */
+
 var numIslands = function(grid) {
-    if (grid.length == 0) {
-        return 0;
-    }
+    var islandCount = 0;
     
-    var width = grid[0].length;
-    var height = grid.length;
-    var numIslands = 0;
-    
-    var mem = new Array(height).fill().map(e => new Array(width).fill(0)); // store state of explored squares
-    
-    for (let r = 0; r < height; ++r) {
-        for (let c = 0; c < width; ++c) {
-            if (mem[r][c] == '0' && grid[r][c] == '1') { // if unexplored island found
-                traverse(r, c, grid, mem);
-                numIslands++;
+    for (let r = 0; r < grid.length; ++r) {
+        for (let c = 0; c < grid[0].length; ++c) {
+            if (grid[r][c] == '1') {          // if unexplored island found
+                DFS(grid, r, c);              // explore whole island
+                islandCount++;                // increment number of islands
             }  
         }
     }
     
-    return numIslands;
-    
+    return islandCount;
 };
 
-var traverse = function(row, col, grid, mem) {
-    if (grid[row][col] == '1') {
-        mem[row][col] = 1;
+var DFS = function(grid, row, col) {
+    // base case: out of bounds or ocean block
+    if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0') {
+        return; 
     }
-    else {
-        return; // stop traversing if hit ocean
-    }
-    
-    // check up
-    if (row >= 1 && mem[row-1][col] == '0') {
-        traverse(row-1, col, grid, mem);
-    }
-    
-    // check left
-    if (col >= 1 && mem[row][col-1] == '0') {
-        traverse(row, col-1, grid, mem);
-    }
-    
-    // check down
-    if (row < grid.length-1 && mem[row+1][col] == '0') {
-        traverse(row+1, col, grid, mem);
-    }
-    
-    // check right
-    if (col < grid[0].length-1 && mem[row][col+1] == '0') {
-        traverse(row, col+1, grid, mem);
-    }
-    
-    return;
+
+    grid[row][col] = '0'; // set island piece to 0  
+
+    DFS(grid, row+1, col); // check down
+    DFS(grid, row-1, col); // check up
+    DFS(grid, row, col+1); // check right
+    DFS(grid, row, col-1); // check left
 }
