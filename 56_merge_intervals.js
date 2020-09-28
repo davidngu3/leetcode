4319542f600250ac324422df69b66e826ec32223
davidngu3 <d.nguyen.melb@gmail.com>
2020-09-28 11:33:54 +1000

    56 tiny bit more optimized

--- 56_merge_intervals.js
+++ 56_merge_intervals.js
@@ -14,15 +14,12 @@ var merge = function(intervals) {
     intervals.push([Infinity, Infinity]); // dummy final value so that last interval is always pushed
     
     for (let i = 1; i < intervals.length; ++i) {
-        if (intervals[i][0] <= end && intervals[i][1] <= end) { // this interval is completely overlapped, skip this interval
-            continue;
-        }
-        else if (intervals[i][0] <= end) { // this interval extends the current interval
-            end = intervals[i][1];
-        }
-        else { // this interval is separate; create new pointers
+        if (intervals[i][0] > end ) { // this interval is separate; create new pointers
             ret.push([start, end]);
             start = intervals[i][0];
+            end = intervals[i][1];
+        }
+        else if (intervals[i][0] <= end && intervals[i][1] > end) { // the current interval can be extended
             end = intervals[i][1];
         }
     }
