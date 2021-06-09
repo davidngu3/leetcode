a62dcf7fa9bb21d429f5c0d9d9218a708894f93e
davidngu3 <d.nguyen.melb@gmail.com>
2021-06-09 22:59:30 +1000

    design hashmap with array

---
+++ leetcode_design_hashmap.py
@@ -0,0 +1,43 @@
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashset = [None for i in range(1000)]
+        
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        hashvalue = key % 1000
+        self.hashset[hashvalue] = value
+        
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        hashvalue = key % 1000
+        if self.hashset[hashvalue]:
+            return self.hashset[hashvalue]
+        
+        return -1
+        
+        
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        hashvalue = key % 1000
+        self.hashset[hashvalue] = None
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
