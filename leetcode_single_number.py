d4a429a2a0304530c0a1c9cb3351ee38f38b81c0
davidngu3 <d.nguyen.melb@gmail.com>
2021-06-09 23:18:58 +1000

    lc single number using hashset

---
+++ leetcode_single_number.py
@@ -0,0 +1,16 @@
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashset = set()
+        
+        for num in nums:
+            if num in hashset:
+                hashset.remove(num)
+            else:
+                hashset.add(num)
+        
+        
+        for val in hashset:
+            return val
