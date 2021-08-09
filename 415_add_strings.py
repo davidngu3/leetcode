2eb716d08f0742e3427ea4a6421d264f80aca21a
davidngu3 <d.nguyen.melb@gmail.com>
2021-08-09 20:22:33 +1000

    415 pt2

--- 415_add_strings.py
+++ 415_add_strings.py
@@ -20,3 +20,40 @@ class Solution:
             res.append(carry)
         
         return ''.join(str(x) for x in res[::-1])
+
+# Solution 2
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        output = ""
+        
+        p1 = len(num1) - 1
+        p2 = len(num2) - 1
+        carry = 0
+        
+        while p1 >= 0 or p2 >= 0:
+        
+            if p1 < 0: 
+                p1val = 0
+                p2val = num2[p2]
+            elif p2 < 0:
+                p1val = num1[p1]
+                p2val = 0
+            else:
+                p1val = num1[p1]
+                p2val = num2[p2]
+            
+            sum = int(p1val) + int(p2val) + carry
+            
+            val = sum % 10
+            carry = sum // 10
+            
+            output = str(val) + output
+            
+            p1 -= 1
+            p2 -= 1
+                
+        if carry:
+            output = str(carry) + output
+        
+        return output
+        
