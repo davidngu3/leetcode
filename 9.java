7660bd7e678c49f045a5af85b278ae031e25b6d0
David <d.nguyen.melb@gmail.com>
2022-01-05 23:32:14 +1100

    int palindrome

--- 9.java
+++ 9.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public static void main(String[] args) {
-        isPalindrome(55255);
+        System.out.println(isPalindromeNoConversion(55));
     }
 
     public static boolean isPalindrome(int x) {
@@ -10,5 +10,21 @@ class Solution {
         StringBuilder sb = new StringBuilder(xString);
         String reversed = sb.reverse().toString();
         return Objects.equals(xString, reversed);
+    }
+
+    public static boolean isPalindromeNoConversion(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        // build the reverse of x by taking mod and dividing
+        int reversed = 0;
+        int original = x;
+        while (original != 0) {
+            reversed = reversed * 10 + original % 10;
+            original = original / 10;
+        }
+        
+        return Integer.compare(x, reversed) == 0;
     }
 }
