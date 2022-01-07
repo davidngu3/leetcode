3e0bbfd8bd49992addf9b8967715c2765ea01343
David <d.nguyen.melb@gmail.com>
2022-01-08 07:25:46 +1100

    same tree

---
+++ 100.java
@@ -0,0 +1,25 @@
+class TreeNode100 {
+    int val;
+    TreeNode100 left;
+    TreeNode100 right;
+    TreeNode100() {}
+    TreeNode100(int val) { this.val = val; }
+    TreeNode100(int val, TreeNode100 left, TreeNode100 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isSameTree(TreeNode100 p, TreeNode100 q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
