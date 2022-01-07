0058f405c06711b0bcf43dbeccd47c8e45c21f04
David <d.nguyen.melb@gmail.com>
2022-01-07 13:47:35 +1100

    q

--- 58.java
+++ 58.java
@@ -1,8 +1,28 @@
 import java.util.*;
 
 class Solution58 {
-    public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        return arr[arr.length-1].length();
+    public static void main(String[] args) {
+        lengthOfLastWord("Hello World ");
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int i = s.length()-1;
+
+        // go to first non-space char
+        char[] arr = s.toCharArray();
+        while (i >= 0 && arr[i] == ' ') {
+            i--;
+        }
+
+        int counter = 0;
+
+        // increment counter until next space or end of string
+        while (i >= 0 && arr[i] != ' ') {
+            counter++;
+            i--;
+        }
+
+
+        return counter;
     }
 }
