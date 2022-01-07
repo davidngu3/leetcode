470e3781775635f931b2237d865505bffb1d3db3
David <d.nguyen.melb@gmail.com>
2022-01-07 11:42:36 +1100

    27

--- 27.java
+++ 27.java
@@ -1,27 +1,19 @@
 class Solution27 {
     public static void main(String[] args) {
-        removeElement(new int[] {6, 5, 2, 2, 3, 2, 5}, val)
+        removeElement(new int[] {6, 5, 2, 2, 3, 2, 5}, 2);
     }
     public static int removeElement(int[] nums, int val) {
-        int i = 0;
-        int p = 0;
-
-        while (p < nums.length) {
-            if (nums[p] >= 0) {
-                int candidate = nums[p];
-                for (int j = p + 1; j < nums.length; j++) {
-                    if (Integer.compare(candidate, nums[j]) == 0) {
-                        nums[j] = -1;
-                    }
-                }
-                nums[p] = -1;
-                nums[i] = candidate;
-                i++;
+        int p = 0; // where to put next element
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!(Integer.compare(nums[i], val) == 0)) {
+                int temp = nums[i];
+                nums[i] = val;
+                nums[p] = temp;
+                p++;
             }
-            p++;
         }
 
-        
-        return i;
+        return p;
     }
 }
