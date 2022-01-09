0cb8cac076b6b717b5f9f17254cd9fbd901f39d8
David <d.nguyen.melb@gmail.com>
2022-01-09 17:04:56 +1100

    111 dfs

--- 111.java
+++ 111.java
@@ -15,15 +15,21 @@ import java.util.*;
  *     }
  * }
  */
-class Solution {
-    public int minDepth(TreeNode root) {
+
+class Solution111 {
+    public static void main(String[] args) {
+        TreeNode ex = TreeNode.createTreeNodeFromArray(new ArrayList<Integer>(Arrays.asList(0, 2, 4, 1, null, 3, -1, 5, 1, null, null, null, 6, null, 8)));
+
+        minDepth(ex);
+    }
+
+    public static int minDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
         
         Deque<NodeWithHeight> queue = new LinkedList<NodeWithHeight>();
-        int currHeight = 1;
-        queue.addLast(new NodeWithHeight(root, currHeight));
+        queue.addLast(new NodeWithHeight(root, 1));
         
         while (!queue.isEmpty()) {
             NodeWithHeight curr = queue.removeFirst();
@@ -31,17 +37,17 @@ class Solution {
             if (curr.node.left == null && curr.node.right == null) {
                 return curr.height;
             }
-            
-            if (root.left != null) { 
-                queue.addLast(new NodeWithHeight(root.left, currHeight + 1)); 
+        
+            if (curr.node.left != null) { 
+                queue.addLast(new NodeWithHeight(curr.node.left, curr.height + 1)); 
             }
-            if (root.right != null) {
-                queue.addLast(new NodeWithHeight(root.right, currHeight + 1));
+            if (curr.node.right != null) {
+                queue.addLast(new NodeWithHeight(curr.node.right, curr.height + 1));
             }
         }
         
         return -1;
-    }a
+    }
 }
 
 class NodeWithHeight {
