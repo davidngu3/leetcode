61ab06354802eb00ad3782d44d149a251a88a21a
David <d.nguyen.melb@gmail.com>
2022-01-09 16:04:28 +1100

    minDepth

--- 111.java
+++ 111.java
@@ -13,25 +13,24 @@
  *     }
  * }
  */
-class Solution110 {
-    public boolean isBalanced(TreeNode root) {
+class Solution111 {
+    public int minDepth(TreeNode root) {
         if (root == null) {
-            return true;
-        }
-        
-        if (Math.abs(height(root.left) - height(root.right)) > 1) {
-            return false;
-        };
-        
-        return isBalanced(root.left) && isBalanced(root.right);
-    }
-    
-    
-    public int height(TreeNode node) {
-        if (node == null) {
             return 0;
         }
         
-        return Math.max(height(node.left), height(node.right)) + 1;
+        return minDepthAux(root);
+    }
+    
+    public int minDepthAux(TreeNode root) {
+        if (root == null) {
+            return 100001;        
+        }
+        
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        return 1 + Math.min(minDepthAux(root.left), minDepthAux(root.right));
     }
 }
