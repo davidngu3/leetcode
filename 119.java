4706ed1565c821b39adb229dff9a88aa630487ca
David <d.nguyen.melb@gmail.com>
2022-01-10 15:24:19 +1100

    pascal 2

--- 119.java
+++ 119.java
@@ -2,41 +2,21 @@ import java.util.*;
 
 class Solution119 {
     public static void main(String[] args) {
-        generate(5);
+        getRow(5);
     }
 
-    public static List<Integer> generate(int rowIndex) {
-        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> ret = new ArrayList<>();
 
-        // first row (base) initialization
-        List<Integer> firstRow = new ArrayList<Integer>(Arrays.asList(1));
-        ret.add(firstRow);
-
-        List<Integer> row, prevRow;
-
-        for (int i = 1; i <= rowIndex; i++) {
-            row = new ArrayList<Integer>();
-            prevRow = ret.get(i-1);
-            
-            // add leading one
-            row.add(1);
-
-            // calculate middle digits using sum of prev row
-            for (int j = 0; j < i-1; j++) {
-                row.add(prevRow.get(j) + prevRow.get(j+1));
+        for (int n = 0; n <= rowIndex; n++) {
+            List<Integer> curr = new ArrayList<>(ret);
+            for (int i = 1; i < ret.size(); i++) {
+                ret.set(i, curr.get(i) + curr.get(i-1));
             }
 
-            // add trailing one
-            row.add(1);
-
-            if (i == rowIndex) {
-                return row;
-            }
-
-            // add completed row
-            ret.add(row);
+            ret.add(1);
         }
 
-        return ret.get(0);
+        return ret;
     }
 }
