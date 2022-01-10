b0c1adbad1cc1eadfcbc126412506c166d8acdc1
David <d.nguyen.melb@gmail.com>
2022-01-10 13:38:39 +1100

    112 optimized

--- 112.java
+++ 112.java
@@ -22,20 +22,14 @@ class Solution112 {
     }
 
     public static boolean hasPathSum(TreeNode root, int targetSum) {
-        return nodeLeafSum(root, targetSum, 0);
-    }
-    
-    public static boolean nodeLeafSum(TreeNode root, int targetSum, int currSum) {
         if (root == null) {
             return false;
         }
-        
-        currSum += root.val;
-        
+    
         if (root.left == null && root.right == null) {
-            return currSum == targetSum;
+            return targetSum == 0;
         }
         
-        return nodeLeafSum(root.left, targetSum, currSum) || nodeLeafSum(root.right, targetSum, currSum);
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val); 
     }
 }
