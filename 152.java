0c17f484b8ea72251123490f01c04353385e9333
David <d.nguyen.melb@gmail.com>
2022-01-12 13:05:03 +1100

    ex

--- 152.java
+++ 152.java
@@ -1,5 +1,9 @@
 class Solution152 {
-    public int maxProduct(int[] nums) {
-        
+    public static void main(String[] args) {
+        System.out.println(maxProduct(new int[]{ 2, 3, -2, 4})); // should be 6
+    }
+
+    public static int maxProduct(int[] nums) {
+        return 0;
     }
 }
