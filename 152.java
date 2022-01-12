b78e73280ace52cab62fe8db7dd0c8980c25825e
David <d.nguyen.melb@gmail.com>
2022-01-12 13:56:37 +1100

    max product subarray

--- 152.java
+++ 152.java
@@ -1,9 +1,34 @@
 class Solution152 {
     public static void main(String[] args) {
-        System.out.println(maxProduct(new int[]{ 2, 3, -2, 4})); // should be 6
+        System.out.println(maxProduct(new int[]{ 2, 3, 0, 7, -3, 8, -2, 4})); // should be 6
     }
 
     public static int maxProduct(int[] nums) {
-        return 0;
+        int max = Integer.MIN_VALUE;
+        int curr = 1;
+        
+        // calculate best product from the left
+        curr = 1;
+        for (int i = 0; i < nums.length; i++) {
+            curr *= nums[i];
+            max = Math.max(curr, max);
+
+            if (nums[i] == 0) {
+                curr = 1;
+            }
+        }
+
+        // calculate best product from the right
+        curr = 1;
+        for (int i = nums.length-1; i >= 0; i--) {
+            curr *= nums[i];
+            max = Math.max(curr, max);
+
+            if (nums[i] == 0) {
+                curr = 1;
+            }
+        }
+
+        return max;
     }
 }
