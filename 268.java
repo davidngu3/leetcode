538623f0762c3dabbdaff5415c854243652da58f
David <d.nguyen.melb@gmail.com>
2022-01-16 10:02:20 +1100

    c

--- 268.java
+++ 268.java
@@ -1,3 +1,10 @@
+// strategy:
+// XOR all numbers 0 to n with all elements in the array
+// e.g. [3, 0, 1]
+// perform 0 ^ 1 ^ 2 ^ 3 ^ 3 ^ 0 ^ 1
+// duplicate elements cancel out i.e. a ^ a = 0
+// so we will be left with the element that does not appear twice (2)
+
 class Solution268 {
     public static void main(String[] args) {
         
