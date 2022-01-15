a12c8e3ad39357a293fd4238f6de85f1ae56c2e0
David <d.nguyen.melb@gmail.com>
2022-01-16 00:16:08 +1100

    alternate method

--- 191.java
+++ 191.java
@@ -1,14 +1,25 @@
 class Solution191 {
     public static void main(String[] args) {
-        hammingWeight(16);
+        hammingWeight1(16);
     }
-    
-    public static int hammingWeight(int n) {
+
+    public static int hammingWeight1(int n) {
         int weight = 0;
 
         while (n != 0) {
             n = n & (n - 1);
             weight++;
+        }
+
+        return weight;
+    }
+
+    public static int hammingWeight2(int n) {
+        int weight = 0;
+
+        while (n != 0) {
+            weight += n & 1;
+            n >>>= 1;
         }
 
         return weight;
