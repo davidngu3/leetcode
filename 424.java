62ce61a2726277449ae0f5ba8c0804001b0fbb88
David <d.nguyen.melb@gmail.com>
2022-01-17 13:38:22 +1100

    char replacement

--- 424.java
+++ 424.java
@@ -2,51 +2,37 @@ import java.util.HashMap;
 
 class Solution424 {
     public static void main(String[] args) {
-        System.out.println(characterReplacement("MJSDSSMESSTR", 2)); // expected 6
+        System.out.println(characterReplacement("ABAB", 2)); // expected 6
     }
 
     public static int characterReplacement(String s, int k) {
-        int i = 0;
-        int j = 0;
-        int maxLength = 0;
-        
         char[] chars = s.toCharArray();
+
         HashMap<Character, Integer> map = new HashMap<>(); // <char, frequency>
-        while (j < s.length()) {
-            
-            // add char to map
-            if (!map.containsKey(chars[j])) {
-                map.put(chars[j], 1);
-            }
-            else {
-                map.put(chars[j], map.get(chars[j])+1);
-            }
-            
-            // check condition holds for any char
-            boolean cond = false;
-            for (char key : map.keySet()) {
-                if ((j-i+1) - map.get(key) <= k) {
-                    cond = true;
-                }
-            }
 
-            // shift left pointer if condition doesnt hold
-            if (!cond) {
-                char curr = chars[i]; // char to transition from
+        int i = 0;
+        int maxLength = 0;
+        int maxRepeat = 0;
 
-                while (chars[i] == curr) {
-                    map.put(curr, map.get(curr) - 1);
-                    i++;
-                }
-                
-                if (map.get(curr) <= 0) {
-                    map.remove(curr);
-                }
-            }
+        for (int j = 0; j < s.length(); j++) {
+            char c = chars[j];
+            // add new char to map
+            if (!map.containsKey(c)) {
+                map.put(c, 0);
+            } 
+            map.put(c, map.get(c) + 1);
+
+            // update max repeats (NOTE: update when new char frequency BEATS historical max)
+            maxRepeat = Math.max(maxRepeat, map.get(c));
+        
+            // if condition doesnt hold, shift left pointer
+            if ((j - i + 1) - maxRepeat > k) {
+                map.put(chars[i], map.get(chars[i]) - 1);
+                i++;
+            } 
             
-            // shift right pointer and update max
+            // update max window length
             maxLength = Math.max(maxLength, j - i + 1);
-            j++;
         }
 
         return maxLength;
