6d8492677d2b38bbf71dc0cfda65d9eec401224c
David <d.nguyen.melb@gmail.com>
2022-01-18 10:58:49 +1100

    is palindrome regex reverse

--- 125.java
+++ 125.java
@@ -1,11 +1,19 @@
-// /*
-//     A phrase is a palindrome if, after converting all uppercase letters 
-//     into lowercase letters and removing all non-alphanumeric characters, 
-//     it reads the same forward and backward. 
-//     Alphanumeric characters include letters and numbers.
-// */
-// class Solution125 {
-//     public boolean isPalindrome(String s) {
-        
-//     }
-// }
+import java.util.*;
+
+class Solution125 {
+    public boolean isPalindrome(String s) {
+        // filtered original string
+        String a = s.replaceAll("[^A-Za-z0-9]", "");
+        String b = a.toLowerCase();
+
+        // edge case
+        if (b.length() == 0 || b.length() == 1) {
+            return true;
+        }
+
+        // reversed string
+        String c = new StringBuilder(b).reverse().toString();
+
+        return Objects.equals(b, c);
+    }
+}
