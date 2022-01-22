4ec1b1480cca1112f3dda7142c07ae3c5b8b5443
David <d.nguyen.melb@gmail.com>
2022-01-22 11:14:57 +1100

    1143 cleaner solution

--- 1143.java
+++ 1143.java
@@ -11,42 +11,19 @@ class Solution1143 {
             return 0;
         }
         
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[m+1][n+1];
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {                
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    if (i == 0 && j == 0) {
-                        dp[i][j] = 1;
-                    }
-                    else if (i == 0) {
-                        dp[i][j] = Math.max(dp[i][j-1], 1);
-                    }
-                    else if (j == 0) {
-                        dp[i][j] = Math.max(dp[i-1][j], 1);
-                    }
-                    else {
-                        dp[i][j] = Math.max(Math.max(dp[i-1][j], dp[i][j-1]), 1 + dp[i-1][j-1]);
-                    }
-                    
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) { 
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else {
-                    if (i == 0 && j == 0) {
-                        dp[i][j] = 0;
-                    }
-                    else if (i == 0) {
-                        dp[i][j] = dp[i][j-1];
-                    }
-                    else if (j == 0) {
-                        dp[i][j] = dp[i-1][j];
-                    }
-                    else {
-                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                    }
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
         
-        return dp[m-1][n-1];
+        return dp[m][n];
     }
 }
