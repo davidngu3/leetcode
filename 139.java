35097d932526b6a6b173e694a6b490cd07003585
David <d.nguyen.melb@gmail.com>
2022-01-23 12:11:11 +1100

    read input into hashmap

--- 139.java
+++ 139.java
@@ -2,24 +2,29 @@ import java.util.*;
 
 class Solution139 {
     public static void main(String[] args) {
-        
+
     }
 
     public static boolean wordBreak(String s, List<String> wordDict) {
+        HashMap<String, Boolean> dict = new HashMap<>();
+        for (String word : wordDict) {
+            dict.put(word, true);
+        }
+
         int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1; // empty string can be made
-        
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true; // empty string can be made
+
         for (int i = 1; i < n + 1; i++) {
-            
-            for (int j = i-1; j >= 0; j--) {
-                
-                if (dp[j] == 1 && wordDict.contains(s.substring(j, i))) {
-                    dp[i] = 1;
+
+            for (int j = i - 1; j >= 0; j--) {
+
+                if (dp[j] == true && dict.containsKey(s.substring(j, i))) {
+                    dp[i] = true;
                 }
             }
         }
-        
-        return dp[n] == 1;
+
+        return dp[n];
     }
 }
