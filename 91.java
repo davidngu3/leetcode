e7cdf518ef4ecc6435047590b300c6f64c7797c8
David <d.nguyen.melb@gmail.com>
2022-01-26 15:17:16 +1100

    decode ways

--- 91.java
+++ 91.java
@@ -1,4 +1,14 @@
 class Solution91 {
+    /*
+        Read string s one char at a time
+        If new char cannot be paired with the previous char, it is equivalent to appending it to all the combinations without it
+        If new char is not 0 can be paired with previous char, introducing a double digit val 11 <= x <= 26, it is equivalent to appending single digit to combinations without it,
+            plus appending double digit to combinations without the double digit
+        If new char is 0 and can be paired with previous char, it is equivalent to appending the double digit to combinations without the double digit.
+
+        memo[i-1] represents the number of ways to decode s[0:i-1]
+        memo[1-2] represents the number of ways to decode s[0:i-2]
+    */
     public static void main(String[] args) {
         numDecodings("2611055971756562");    
     }
@@ -18,12 +28,12 @@ class Solution91 {
             int currChar = Character.getNumericValue(c[i]);
             int prevChar = Character.getNumericValue(c[i-1]);
 
-            // invalid case: 0 cannot be paired
+            // invalid case: 0 cannot be paired with previous number
             if (currChar == 0 && (prevChar == 0 || prevChar > 2)) {
                 return 0;
             }
             
-            // reduce way case: 0 must be attached, cannot be standalone
+            // double digit append: 0 must be attached to previous number, cannot be standalone
             if (currChar == 0) {
                 if (i == 1) {
                     memo[i] = 1;
@@ -33,7 +43,7 @@ class Solution91 {
                 }
             }   
 
-            // new way case: new char makes a double digit valid numeral
+            // single digit + double digit append: new char makes a double digit valid numeral
             else if (prevChar == 1 || prevChar == 2 && currChar <= 6) {
                 if (i == 1) {
                     memo[i] = memo[i-1] + 1;
@@ -43,6 +53,7 @@ class Solution91 {
                 }
             }
             
+            // single digit append
             else {
                 memo[i] = memo[i-1];
             }
