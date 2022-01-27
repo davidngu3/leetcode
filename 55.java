eb65285ae574281f076bc164a57719d9d6f624b7
David <d.nguyen.melb@gmail.com>
2022-01-27 14:07:06 +1100

    55 optimized greedy O(1) space

--- 55.java
+++ 55.java
@@ -4,41 +4,22 @@ class Solution55 {
     }
 
     public static boolean canJump(int[] nums) {
-        // edge cases
+       int reachable = 0;
 
-        if (nums.length == 1) {
-            return true;
-        } 
-        
-        if (nums.length == 2) {
-            return nums[0] > 0;
-        }
+       for (int i = 0; i < nums.length; i++) {
+           // early failure condition
+           if (i > reachable) { 
+               return false;
+           }
+           reachable = Math.max(reachable, i + nums[i]);
 
-        if (nums[0] == 0) {
-            return false;
-        }
+           // early success condition
+           if (reachable >= nums.length - 1) {
+               return true;
+           }
+       }
 
-        // stores furthest reachable index
-        int[] dp = new int[nums.length]; 
-        dp[0] = nums[0];
-
-        for (int i = 1; i < nums.length - 1; i++) {
-            // early failure condition: cannot jump past zero
-            if (nums[i] == 0 && dp[i-1] <= i) {
-                return false;
-            }
-            
-            // calculate max reachable index
-            dp[i] = Math.max(dp[i-1], i+nums[i]);
-
-            // early success condition: reachable index at/past final index
-            if (dp[i] >= nums.length - 1) {
-                return true;
-            }
-            
-        }
- 
-        return false;
-
+       // not hit: code will reach one of two early conditions
+       return true;
     }
 }
