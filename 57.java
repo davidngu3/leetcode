bd67b3491de0647cf163b2ded3956f2ebaaa49dc
David <d.nguyen.melb@gmail.com>
2022-01-29 15:46:44 +1100

    insert interval

--- 57.java
+++ 57.java
@@ -2,83 +2,54 @@ import java.util.*;
 
 class Solution57 {
     public static void main(String[] args) {
-        int[][] intervals = new int[2][2];
+        int[][] intervals = new int[3][2];
         
-        intervals[0] = new int[] {1, 5};
+        intervals[0] = new int[] {2, 5};
+        intervals[1] = new int[] {6, 7};
+        intervals[2] = new int[] {8, 9};
        
-        int[] newInterval = new int[] {1, 7}; 
+        int[] newInterval = new int[] {0, 1}; 
         insert(intervals, newInterval);
     }
 
     public static int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> ret = new ArrayList<int[]>();
-        int start = newInterval[0];
-        int end = newInterval[1];
+        // array to hold solution intervals
+        List<int[]> sol = new ArrayList<int[]>();
+
+        boolean newIntervalPlaced = false;
         
-        // temp to hold new interval
-        int[] curr = new int[2];
-        boolean mergeInProcess = false;
-        
-        // iterate intervals, check condition
-        for (int[] interval : intervals) {
-            if (mergeInProcess) {
-                // merge interval and continue
-                if (interval[0] <= curr[1]) {
-                    curr[1] = Math.max(curr[1], interval[1]);
+        // build solution intervals
+        for (int[] interval: intervals) {
+            // new interval precedes entirely
+            if (newInterval[1] < interval[0]) {
+                if (!newIntervalPlaced) {
+                    sol.add(newInterval);
+                    newIntervalPlaced = true;
                 }
-                
-                // complete merge
-                else {
-                    ret.add(curr);
-                    ret.add(interval);
-                    mergeInProcess = false;
-                }
+                sol.add(interval);
             }
-            
+
+            // new interval succeeds entirely
+            else if (interval[1] < newInterval[0]) {
+                sol.add(interval);
+            }
+
+            // new interval overlap
             else {
-                // case 1: interval occurs entirely before 
-                if (interval[1] < start) {
-                    ret.add(interval);
-                }
-    
-                // case 2: interval occurs entirely after
-                else if (interval[0] > end) {
-                    ret.add(interval);
-                }
-                
-                // case 3 full coincidence
-                else if (interval[0] <= start && interval[1] >= end) {
-                    return intervals;
-                }
-
-                // case 4: end extension / merge
-                else if (start <= interval[1] && interval[0] < start) {
-                    curr[0] = interval[0];
-                    curr[1] = end;
-                    mergeInProcess = true; 
-                }
-
-                // case 5: start extension
-                else if (start < interval[0] && end < interval[1]) {
-                    curr[0] = start;
-                    curr[1] = interval[1];
-                    ret.add(curr);
-                }
+                newInterval = new int[] { Math.min(newInterval[0], interval[0]), Math.max(newInterval[1], interval[1]) };
             }
         }
-
-        // hanging merge
-        if (mergeInProcess) {
-            ret.add(curr);
-        }
         
-        // convert return to int[][] from arraylist
-        int[][] sol = new int[ret.size()][2];
-        for (int i = 0; i < ret.size(); i++) {
-            sol[i] = ret.get(i);
+        if (!newIntervalPlaced) {
+            sol.add(newInterval);
+        }
+
+        // convert return type
+        int[][] ret = new int[sol.size()][2];
+        for (int i = 0; i < sol.size(); i++) {
+            ret[i] = sol.get(i);
         } 
 
-        return sol;
-            
+        return ret;
     }
 }
