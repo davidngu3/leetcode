1d44432b9a3d2b4776371830337224b8cc8d6168
David <d.nguyen.melb@gmail.com>
2022-02-01 12:19:04 +1100

    reverse linked list

---
+++ 206.java
@@ -0,0 +1,43 @@
+class Solution206 {
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+    
+        reverseList(a);    
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        // edge cases
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        // set up pointers
+        ListNode curr = head.next;
+        ListNode prev = head;
+        prev.next = null; // break first link
+        
+        // reverse links iteratively
+        while (curr != null) {
+            // continuation point
+            ListNode temp = curr.next;
+            
+            // reverse link
+            curr.next = prev;
+            
+            // shift pointers
+            prev = curr;
+            curr = temp;
+        }
+        
+        return prev;
+    }
+}
