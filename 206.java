7b6c3528046eb4dadfc68f313e8f6e4e23078915
David <d.nguyen.melb@gmail.com>
2022-02-02 12:01:07 +1100

    cycle detection

--- 206.java
+++ 206.java
@@ -1,42 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 class Solution206 {
-    public static void main(String[] args) {
-        ListNode a = new ListNode(1);
-        ListNode b = new ListNode(2);
-        ListNode c = new ListNode(3);
-        ListNode d = new ListNode(4);
-        ListNode e = new ListNode(5);
+    public boolean hasCycle(ListNode head) {
         
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-    
-        reverseList(a);    
-    }
-
-    public static ListNode reverseList(ListNode head) {
-        // edge cases
-        if (head == null || head.next == null) {
-            return head;
+        // fast pointer goes 2x as fast as slow pointer
+        // if fast and slow meet, there's a cycle
+        // if either is null, no cycle
+        
+        ListNode fast = head;
+        
+        while (fast != null && head != null) {
+            if (fast == null || fast.next == null || head == null) {
+                return false;
+            }
+            
+            // traverse pointers
+            fast = fast.next.next;
+            head = head.next;
+            
+            // check cycle condition
+            if (fast == head) {
+                return true;
+            }    
+            
+            
         }
         
-        // set up pointers
-        ListNode curr = head;
-        ListNode prev = null; 
-        
-        // reverse links iteratively
-        while (curr != null) {
-            // continuation point
-            ListNode temp = curr.next;
-            
-            // reverse link
-            curr.next = prev;
-            
-            // shift pointers
-            prev = curr;
-            curr = temp;
-        }
-        
-        return prev;
+        // not hit
+        return false;
     }
 }
