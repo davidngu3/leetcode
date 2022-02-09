4715d1c88c562d3500d218b8c39758964e9bd22b
David <d.nguyen.melb@gmail.com>
2022-02-09 11:22:55 +1100

    matrix zeros solution O(N)

--- 73.java
+++ 73.java
@@ -19,5 +19,32 @@ class Solution73 {
 
         // O(MN) time complexity O(1) space
 
+        // first pass: place flags
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                // place flag
+                if (matrix[r][c] == 0) {
+                    matrix[0][c] = 0;
+                    matrix[r][0] = 0;
+                }
+            }
+        }
+
+        // second pass: process flags
+        for (int r = 0; r < matrix.length; r++) {
+            if (matrix[r][0] == 0) {
+                for (int c = 0; c < matrix[0].length; c++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        for (int c = 0; c < matrix[0].length; c++) {
+            if (matrix[0][c] == 0) {
+                for (int r = 0; r < matrix.length; r++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
     }
 }
